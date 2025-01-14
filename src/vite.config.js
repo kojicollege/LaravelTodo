ab52d440111155762b510c2e59b2a2a0b7e4b6d3
crazymaki <katami4koji.923@gmail.com>
2025-01-14 23:52:46 +0900

    tailwindcssの導入

--- src/vite.config.js
+++ src/vite.config.js
@@ -1,11 +1,23 @@
-import { defineConfig } from 'vite';
-import laravel from 'laravel-vite-plugin';
+import { defineConfig } from "vite";
+import laravel from "laravel-vite-plugin";
 
 export default defineConfig({
     plugins: [
         laravel({
-            input: ['resources/css/app.css', 'resources/js/app.js'],
+            input: ["resources/css/app.css", "resources/js/app.js"],
             refresh: true,
         }),
     ],
+    server: {
+        hmr: {
+            host: "localhost", // ホスト名を明示的に指定
+            protocol: "ws", // WebSocket プロトコルの使用
+            port: 5173, // デフォルトポート（Viteがリッスンするポート）
+        },
+        watch: {
+            usePolling: true, // ポーリングを有効にして、ファイル変更を監視
+        },
+        host: "0.0.0.0", // Dockerコンテナで外部からアクセスできるように設定
+        port: 5173, // 使用するポート番号
+    },
 });
